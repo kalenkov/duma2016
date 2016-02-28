912d1dcb3d5b054f8b7760b6c0049b1f6127bd8d
Mikhail Kalenkov <mikhail.kalenkov@gmail.com>
2016-02-28 14:25:57 +0300

    Обрезка по береговой линии

--- id2Makefile.pl
+++ id2Makefile.pl
@@ -14,6 +14,25 @@ my $all_land_make="\trm -f okrug_all_diss_land.*\n";
 my $clean_source="";
 my $clean_all_source="";
 
+#Граница России
+$id="60189";
+print "$id.osm:\n";
+print "\twget -O $id.osm \"http://www.openstreetmap.org/api/0.6/relation/$id/full\"\n";
+print "russia.osm: $id.osm\n";
+print "\tosmfilter $id.osm --drop-tags=\"name*=\" -o=russia.osm\n";
+print "russia.sqlite: russia.osm\n";
+print "\togr2ogr -f SQLite -dsco spatialite=yes russia.sqlite russia.osm --config OGR_SQLITE_SYNCHRONOUS OFF --config OSM_USE_CUSTOM_INDEXING NO\n";
+print "russia.shp: russia.sqlite\n";
+print "\togr2ogr -f \"ESRI Shapefile\" russia.shp russia.sqlite -sql \"select geometry from multipolygons\"\n";
+print "russia_land.shp: russia.shp land_polygons.shp\n";
+print "\togr2ogr -dialect SQLITE -sql \"SELECT ST_Intersection(A.geometry, B.geometry) AS geometry, A.*, B.* FROM land_polygons A, russia B WHERE ST_Intersects(A.geometry, B.geometry)\" . . -nln russia_land\n";
+print "russia_land_diss.shp: russia_land.shp\n";
+print "\togr2ogr russia_land_diss.shp russia_land.shp -dialect sqlite -sql \"SELECT ST_Union(geometry) AS geometry FROM russia_land\"\n";
+print "clean_russia:\n";
+print "\trm -f russia.osm russia.sqlite russia.shp russia.shx russia.dbf russia.prj russia_land.shp russia_land.shx russia_land.dbf russia_land.prj russia_land_diss.shp russia_land_diss.shx russia_land_diss.dbf russia_land_diss.prj \n";
+print "clean_all_russia: clean_russia\n";
+print "\trm -f $id.osm\n\n";
+
 #Синьяльское сельское поселение (5522997)
 $id="5522997";
 print "$id.osm:\n";
@@ -75,8 +94,8 @@ while (defined($line = <STDIN>)) {
 		print "\togr2ogr okrug_$okrug\_diss.shp okrug_$okrug.shp -dialect sqlite -sql \"SELECT ST_Union(geometry) AS geometry FROM okrug_$okrug\"\n";
 		print "\togrinfo okrug_$okrug\_diss.shp -sql \"ALTER TABLE okrug_$okrug\_diss ADD COLUMN okrug integer(4)\"\n";
 		print "\togrinfo okrug_$okrug\_diss.shp -dialect SQLite -sql \"UPDATE okrug_$okrug\_diss SET okrug = $okrug\"\n";
-		print "okrug_$okrug\_diss_land.shp: okrug_$okrug\_diss.shp\n";
-		print "\togr2ogr -f \"ESRI Shapefile\" okrug_$okrug\_diss_land.shp -clipsrc okrug_$okrug\_diss.shp land_russia.shp\n";
+		print "okrug_$okrug\_diss_land.shp: okrug_$okrug\_diss.shp russia_land_diss.shp\n";
+		print "\togr2ogr -f \"ESRI Shapefile\" okrug_$okrug\_diss_land.shp -clipsrc russia_land_diss.shp okrug_$okrug\_diss.shp\n";
 		print "okrug_$okrug.shp: $shp_source\n";
 		print "\trm -f okrug_$okrug.shp okrug_$okrug.shx okrug_$okrug.dbf okrug_$okrug.prj\n";
 		print "$shp_make";
@@ -94,12 +113,16 @@ while (defined($line = <STDIN>)) {
 	}
 }
 
-print "all: $all_source\n";
+print "okrug_all: $all_source\n";
 print "$all_make \n";
 
-print "all_land: $all_land_source\n";
+print "okrug_all_land: $all_land_source\n";
 print "$all_land_make \n";
 
-print "clean_all: $clean_all_source\n\n";
+print "clean_all_okrug: $clean_all_source\n\n";
 
-print "clean: $clean_source\n\n";
+print "clean_all: clean_all_okrug clean_all_russia\n\n";
+
+print "clean_okrug: $clean_source\n\n";
+
+print "clean: clean_okrug clean_russia\n\n";
